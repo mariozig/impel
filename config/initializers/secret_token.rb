003c4a2fe8312a739d1b556e38894aba3950be86
mariozig <mariozig@gmail.com>
2013-04-29 21:18:14 -0700

    begin.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Impel::Application.config.secret_key_base = '70aa554b409410579ba867103c564f6c449c91291a0ce38b816bbd7c0262f536d9f5b346eb715104ff49ebe735bae63e9b01b3fa63d563b5ce3cff0b8c68ec04'
