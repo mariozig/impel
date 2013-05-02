7627ff4e0a22b0a4f54bb2530bdb8ab068e2c17f
mariozig <mariozig@gmail.com>
2013-05-02 10:43:17 -0700

    Paginate the post listing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.order("created_at DESC")
+    @posts = Post.order("created_at DESC").page(params[:page]).per(25)
   end
 end
